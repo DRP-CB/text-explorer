9dcec90b121b1f3696d24f1d6ad4475af52457a6
paul <paul.deruta@gmail.com>
2022-05-31 15:46:26 +0200

    Création du concordancier. Début d'implémentation des 3 composants en UI

---
+++ dash_textbox.py
@@ -0,0 +1,52 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
+from text_explorer_functions import get_syntagmatic_data, plot_word_of_interest
+from arango import ArangoClient
+
+client = ArangoClient(hosts="http://localhost:8529")
+sys_db = client.db('_system', username='root',password='root')
+db = client.db("text", username="root", password="root")
+
+syntagmatic_plot = plot_word_of_interest('classe')
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id='syntagmatic_graph',
+              figure = syntagmatic_plot),
+    html.Div(id='graph_output', style={'whiteSpace': 'pre-line'}),
+
+    dcc.Textarea(
+        id='textarea-example',
+        value='doc0sent0',
+        style={'width': '100%', 'height': 300},
+    ),
+    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
+])
+
+
+@app.callback(
+    Output('graph_output','children'),
+    Input('syntagmatic_graph','clickData')
+)
+def return_click(value):
+    if value is None:
+        return 'sélectionner un point'
+    else :
+        return value['points'][0]['text']
+    
+@app.callback(
+    Output('textarea-example-output', 'children'),
+    Input('textarea-example', 'value')
+)
+def return_phrase(key):
+    sent = list(db.aql.execute(f'''
+    FOR sent in sentences
+    FILTER sent._key == {"'"+ key +"'"}
+    RETURN sent.content
+'''))[0]
+    return sent
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
